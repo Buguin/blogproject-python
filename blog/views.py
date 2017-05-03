12a42e8138e35eb21e33f3b68cb5a7592edd43dd
Buguin <wybuguin@hotmail.com>
2017-05-03 23:05:12 +0800

    13 评论
    评论相关功能添加

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ import markdown
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category
+from comments.forms import CommentForm
 
 # Create your views here.
 
@@ -20,8 +21,14 @@ def detail(request, pk):
                                       'markdown.extensions.codehilite',
                                       'markdown.extensions.toc',
                                   ])
-
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    #获取所有评论
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
 
 
 def archives(request, year, month):
