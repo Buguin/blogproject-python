1f70d5894616831a9709f488e73656605175fc21
Buguin <wybuguin@hotmail.com>
2017-05-06 03:00:28 +0800

    15 使用 Markdown 自动生成文章目录
    生成文章目录

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category
 from comments.forms import CommentForm
-
-# Create your views here.
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
 
 
 def index(request):
@@ -14,19 +14,18 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    #post.body = markdown.markdown(post.body, ['codehilite'])
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
     form = CommentForm()
-    #获取所有评论
     comment_list = post.comment_set.all()
     context = {'post': post,
                'form': form,
-               'comment_list': comment_list
+               'comment_list': comment_list,
+               'toc': md.toc
                }
     return render(request, 'blog/detail.html', context=context)
 
@@ -41,3 +40,12 @@ def category(request, pk):
     post_list = Post.objects.filter(category=cate)
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
+
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+    if not q:
+        error_msg = '请输入关键词'
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+    post_list = Post.objects.filter(title__icontains=q)
+    return render(request, 'blog/index.html', {'error_msg': error_msg, 'post_list': post_list})
